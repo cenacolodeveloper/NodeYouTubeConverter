33c5e0b0a0df546d916259df00c31f8194affd83
cenacolodeveloper <cenacolodeveloper@gmail.com>
2025-03-11 14:29:25 -0500

    youtube api node test

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
 require('dotenv').config();
+
 
 const API_KEY = process.env.API_KEY;
 
 console.log(API_KEY)
+
+
+// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
+// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
+// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
+
+ytdl('https://www.youtube.com/watch?v=zhrojjhx0oo')
+  .pipe(fs.createWriteStream('video.mp4'));
